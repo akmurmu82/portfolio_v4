d9bbf77fa8380f1ad761edd30728f6b160aa8550
amit murmu <your.akmurmu6580@google.com>
2024-06-23 18:07:02 +0530

    projects added

--- src/Section/Project.jsx
+++ src/Section/Project.jsx
@@ -1,6 +1,9 @@
 import { Box, Grid } from "@chakra-ui/react";
 import MyHeading from "./components/Heading";
 import { ProjectCard } from "./components/ProjectCard";
+import interviewHub from "../images/InterviewHub.png";
+import Todoist from "../images/Todoist.png";
+import GroceryHub from "../images/GroceryHub.png";
 
 function Projects() {
   return (
@@ -15,42 +18,9 @@ function Projects() {
       <Box>
         <MyHeading title="PROJECTS" />
         <Grid templateColumns={{ base: "1fr", lg: "repeat(3, 1fr)" }} gap={10}>
-          <ProjectCard
-            title={"App 1"}
-            image={
-              "https://th.bing.com/th/id/OIP.Dbbsi4_1MA-1IGQRK894sQAAAA?rs=1&pid=ImgDetMain"
-            }
-          />
-          <ProjectCard
-            title={"App 1"}
-            image={
-              "https://th.bing.com/th/id/OIP.Dbbsi4_1MA-1IGQRK894sQAAAA?rs=1&pid=ImgDetMain"
-            }
-          />
-          <ProjectCard
-            title={"App 1"}
-            image={
-              "https://th.bing.com/th/id/OIP.Dbbsi4_1MA-1IGQRK894sQAAAA?rs=1&pid=ImgDetMain"
-            }
-          />
-          <ProjectCard
-            title={"App 1"}
-            image={
-              "https://th.bing.com/th/id/OIP.Dbbsi4_1MA-1IGQRK894sQAAAA?rs=1&pid=ImgDetMain"
-            }
-          />
-          <ProjectCard
-            title={"App 1"}
-            image={
-              "https://th.bing.com/th/id/OIP.Dbbsi4_1MA-1IGQRK894sQAAAA?rs=1&pid=ImgDetMain"
-            }
-          />
-          <ProjectCard
-            title={"App 1"}
-            image={
-              "https://th.bing.com/th/id/OIP.Dbbsi4_1MA-1IGQRK894sQAAAA?rs=1&pid=ImgDetMain"
-            }
-          />
+          <ProjectCard title={"Interview Hub"} image={interviewHub} />
+          <ProjectCard title={"Todoist"} image={Todoist} />
+          <ProjectCard title={"GroceryHub"} image={GroceryHub} />
         </Grid>
       </Box>
     </Box>
